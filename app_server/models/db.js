4f2cd1328988713773df5e83b3b1d8482a37d3a7
Ali-klani <alii.kalantary@gmail.com>
2022-11-10 23:05:52 +0300

    update

---
+++ app_server/models/db.js
@@ -0,0 +1,31 @@
+var mongoose= require("mongoose");
+require("./mekansema");
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://ali:ali1234@mekanbul.bwboamp.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+mongoose.connection.close(function(){
+console.log(msg);
+callback();
+}
+);
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+        process.exit(0);
+    }
+    );
+}
+);
+mongoose.connection.on("connected",function(){
+console.log(dbURI+"adresindeki veritabanına bağlandı")
+}
+);
+mongoose.connection.on("disconnected",function(){ 
+console.log(dbURI+"adresindeki veritabanına bağlantısı koptu")
+}
+);
+mongoose.connection.on("error",function(){
+console.log(dbURI+"bağlantı hatası")
+}
+);
